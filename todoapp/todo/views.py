7cbc1ed5b5593022e46e3eec98dbc75122e6ee8c
Elizbeth <elizabethadebayo269@gmail.com>
2025-09-08 04:59:48 +0100

    added display messages functionality

--- todoapp/todo/views.py
+++ todoapp/todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 from django.utils import timezone
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -19,10 +20,12 @@ def createTask(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)   
         if form.is_valid():             
-            form.save()                 
+            form.save()    
+            messages.success(request, "Task created successfully!")             
             return redirect('home')     
     else:
-        form = TaskForm()               
+        form = TaskForm() 
+        messages.error(request, "An error occured")              
     context = {'tasks': tasks, 'form': form}
 
     return render(request, 'todo/createTask.html', context)
@@ -34,7 +37,10 @@ def editTask(request, pk):
         form = TaskForm( request.POST, instance=task)
         if form.is_valid():
             form.save()
+            messages.success(request, "Task updated successfully!")
             return redirect('home')
+        else:
+            messages.error(request, 'An error occured')
         
     context = {
         # 'task': task,
@@ -46,6 +52,7 @@ def deleteTask(request, pk):
     task = Task.objects.get(id=pk)
     if request.method == 'POST':
         task.delete()
+        messages.success(request, "Task deleted successfully!")
         return redirect('home')
     return render(request, 'todo/deleteTask.html', {'obj': pk})
 
@@ -86,9 +93,20 @@ def completedTask(request):
     return render(request, 'completed.html', {'tasks':tasks})
 
 def toggle_completed(request, pk):
-    task = get_object_or_404(Task, id=pk)
-    if request.method == "POST":
-        task.completed = not task.completed
-        # return redirect(request.META.get("HTTP_REFERER", "completed"))
-        task.save()
-    return redirect(request.META.get('HTTP_REFERER', '/'))
+    task = Task.objects.get(id=pk)
+    task.completed = not task.completed
+    task.save()
+    if task.completed:
+        messages.success(request, f"Task '{task.title}' marked as completed!")
+    else:
+        messages.info(request, f"Task '{task.title}' marked as pending!")
+    return redirect(request.META.get("HTTP_REFERER", "/"))
+
+# def toggle_completed(request, pk):
+#     task = get_object_or_404(Task, id=pk)
+#     if request.method == "POST":
+#         task.completed = not task.completed
+#         # return redirect(request.META.get("HTTP_REFERER", "completed"))
+#         task.save()
+#         messages.success(request, "You completed f`(${task})`!")
+#     return redirect(request.META.get('HTTP_REFERER', '/'))
